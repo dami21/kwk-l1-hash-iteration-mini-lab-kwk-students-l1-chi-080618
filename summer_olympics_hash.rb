e491908e24ae99548e8cd9a869f97eea940eedfd
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-08 19:46:52 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,10 +1,12 @@
-summer_olympics = {:Sydney => 2000      
- :Athens  => 2004      
- :Beijing => 2008      
- :London  => 2012 
- :}
- puts 
-def create_olym
+summer_olympics_2 = {:Atlanta => 1996, :Sydney => 2000,       
+ :Athens  => 2004,       
+ :Beijing => 2008,       
+ :London  => 2012,  
+ }
+ summer_olympics_2.each do 
+   puts summer_olympics_2
+   ""
+def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
 end
 
